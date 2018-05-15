3ab76bf9c8944b1a8f4a48627be7e74459457214
horis233 <horis233@gmail.com>
2018-05-15 09:29:09 -0400

    first commit

---
+++ tap-news/src/NewsPanel/NewsPanel.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './NewsPanel.css';
+import NewsCard from '../NewsCard/NewsCard';
+
+class NewsPanel extends React.Component {
+  constructor() {
+    super();
+    this.state = { news: null };
+  }
+
+  componentDidMount(){
+    this.loadMoreNews();
+  }
+
+  loadMoreNews() {
+    this.setState({
+      news : [
+        {....data
+      }]
+    });
+  }
+
+  renderNews() {
+    const news_list = this.state.news.map(news => {
+      return (
+        <a className = 'list-group-item' key = {news.digest} href = '#'>
+          <NewsCard news = {news} />
+        </a>
+      );
+    });
+
+    return (
+      <div className = 'container-fluid'>
+        <div className = "list-group">
+          {news_list}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    if (this.state.news) {
+      return(
+        <div>
+          {this.renderNews()}
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          Loading...
+        </div>
+      );
+    }
+  }
+}
+
+export default NewsPanel;
