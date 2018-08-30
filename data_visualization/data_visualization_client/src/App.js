7e3ef02ccaa20036fa5c2c31e77fc089d75ccd8a
horis233 <horis233@gmail.com>
2018-08-30 00:10:28 -0400

    add Cards with fake data

--- data_visualization/data_visualization_client/src/App.js
+++ data_visualization/data_visualization_client/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Range from './Range/Range.js';
+import Cards from './Cards/Cards.js';
+//import MainContainer from './MainContainer/MainContainer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Cards/>
       </div>
     );
   }
