da98d131648c9acab4be5057d00d07a3bd481387
horis233 <horis233@gmail.com>
2018-09-04 23:17:37 -0400

    add Panel Component

--- data_visualization/data_visualization_client/src/App.js
+++ data_visualization/data_visualization_client/src/App.js
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Range from './Range/Range.js';
 import Cards from './Cards/Cards.js';
-//import MainContainer from './MainContainer/MainContainer.js';
+import MainContainer from './MainContainer/MainContainer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Cards/>
+        <MainContainer/>
       </div>
     );
   }
