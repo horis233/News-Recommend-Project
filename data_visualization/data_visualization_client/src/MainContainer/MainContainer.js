64288d97c2fcfa62b2f58e7a2190feebea35e615
horis233 <horis233@gmail.com>
2018-09-18 23:24:07 -0400

    added device pie chart

--- data_visualization/data_visualization_client/src/MainContainer/MainContainer.js
+++ data_visualization/data_visualization_client/src/MainContainer/MainContainer.js
@@ -5,7 +5,7 @@ import Range from '../Range/Range.js';
 
 import UserTrend from '../UserTrend/UserTrend.js';
 import ActiveUserChart from '../ActiveUserChart/ActiveUserChart.js';
-// import DevicePieChart from '../DevicePieChart/DevicePieChart.js';
+import DevicePieChart from '../DevicePieChart/DevicePieChart.js';
 // import NewsCategory from '../NewsCategory/NewsCategory.js';
 
 const category = ['technology', 'music', 'education','sports','politics', 'weather','others'];
@@ -25,20 +25,20 @@ class MainContainer extends React.Component{
         return(
             <div>
                 <div className="row">
-                    {/* <div className="col-md-5 custom_padding" >
+                    <div className="col-md-5 custom_padding" >
                         <Panel>
                             <PanelHeader title="User Device">
                             </PanelHeader>
                             <DevicePieChart data={this.state.user_device}/>
                         </Panel>
-                    </div> */}
+                    </div>
                     <div className="col-md-7 custom_padding" >
                         <Panel>
                             <PanelHeader title="User Trend (Daily Active/Daily New)">
                             </PanelHeader>
-                            <div>
+                            
                             <ActiveUserChart data={this.state.user_trend} />
-                            </div>
+
                         </Panel>
                     </div>
                 </div>
