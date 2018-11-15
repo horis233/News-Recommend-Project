f64b8bdf1163ec0bdb8e5ecf85c7f20414987074
horis233 <horis233@gmail.com>
2018-11-15 17:38:39 -0500

    update http to https

--- web_server/client/src/NewsPanel/NewsPanel.js
+++ web_server/client/src/NewsPanel/NewsPanel.js
@@ -10,22 +10,22 @@ class NewsPanel extends React.Component {
     super();
     this.state = {
       news: null,
-      pageNum:0,
-      loadedAll:false
+      pageNum: 0,
+      loadedAll: false
     };
     this.handleScroll = this.handleScroll.bind(this);
   }
 
   handleScroll() {
-    const scrollY = window.scrollY
-        || window.pageYOffset
-        || document.documentElement.scrollYTop;
+    const scrollY = window.scrollY ||
+      window.pageYOffset ||
+      document.documentElement.scrollYTop;
     if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
       this.loadMoreNews();
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.loadMoreNews();
     this.loadMoreNews = _.debounce(this.loadMoreNews, 1000);
     window.addEventListener('scroll', () => this.handleScroll());
@@ -36,42 +36,44 @@ class NewsPanel extends React.Component {
       return;
     }
 
-    let url = 'http://' + window.location.hostname + ':3000' +
-        '/news/userId/' + Auth.getEmail() + '/pageNum/' + this.state.pageNum;
+    let url = 'https://' + window.location.hostname +
+      '/news/userId/' + Auth.getEmail() + '/pageNum/' + this.state.pageNum;
 
     let request = new Request(encodeURI(url), {
       method: 'GET',
       headers: {
         'Authorization': 'bearer ' + Auth.getToken(),
       },
-      });
+    });
 
     fetch(request)
       .then(
         res => {
-                if (res.status === 200) {
-                    return res.json();
-                }
+          if (res.status === 200) {
+            return res.json();
+          }
 
-                // TODO: not authenticated; redirect to login page?
-                if (res.status === 401) {
-                    Auth.deAuthenticate();
-                    this.context.router.history.replace('/login');
-                    throw Error('user not authenticated')
-                }
-                if (res.status === 500) {
-                    // TODO: server might send back some error info, but not take it here
-                    throw Error('Fetching news: server error!');
-                }
-                throw Error('Fetching news: other error!')
-            }
+          // TODO: not authenticated; redirect to login page?
+          if (res.status === 401) {
+            Auth.deAuthenticate();
+            this.context.router.history.replace('/login');
+            throw Error('user not authenticated')
+          }
+          if (res.status === 500) {
+            // TODO: server might send back some error info, but not take it here
+            throw Error('Fetching news: server error!');
+          }
+          throw Error('Fetching news: other error!')
+        }
       )
       .then((news_list) => {
-        if (!news_list ||  news_list.length === 0) {
-          this.setState({loadedAll: true});
-        }else {
+        if (!news_list || news_list.length === 0) {
           this.setState({
-            news: this.state.news == null ?  news_list: this.state.news.concat(news_list),
+            loadedAll: true
+          });
+        } else {
+          this.setState({
+            news: this.state.news == null ? news_list : this.state.news.concat(news_list),
             pageNum: this.state.pageNum + 1
           });
         }
@@ -80,43 +82,55 @@ class NewsPanel extends React.Component {
 
   renderNews() {
     const news_list = this.state.news.map(news => {
-      return (
-          <NewsCard news = {news} />
+      return ( <
+        NewsCard news = {
+          news
+        }
+        />
       );
     });
 
-    return (
-        <div className = "row">
-          {news_list}
-        </div>
+    return ( <
+      div className = "row" > {
+        news_list
+      } <
+      /div>
     );
   }
 
   render() {
     if (this.state.news) {
-      return(
-        <div>
-          {this.renderNews()}
-          { this.state.loading===true &&
-              <div className="row">
-              <div className="progress col s12 m6 l4 offset-m3 offset-l4">
-                <div className="indeterminate"></div>
-              </div>
-              </div>
-          }
-        </div>
+      return ( <
+        div > {
+          this.renderNews()
+        } {
+          this.state.loading === true &&
+            <
+            div className = "row" >
+            <
+            div className = "progress col s12 m6 l4 offset-m3 offset-l4" >
+            <
+            div className = "indeterminate" > < /div> <
+            /div> <
+            /div>
+        } <
+        /div>
       );
     } else {
-      return(
-        <div>
-          <div className="row">
-          <div className="bar-div">
-          <div className="progress col s12 m6 l4 offset-m3 offset-l4">
-            <div className="indeterminate"></div>
-          </div>
-          </div>
-          </div>
-        </div>
+      return ( <
+        div >
+        <
+        div className = "row" >
+        <
+        div className = "bar-div" >
+        <
+        div className = "progress col s12 m6 l4 offset-m3 offset-l4" >
+        <
+        div className = "indeterminate" > < /div> <
+        /div> <
+        /div> <
+        /div> <
+        /div>
       );
     }
   }
